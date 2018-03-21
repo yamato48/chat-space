da6b46c4e1b33b8c61af343ce5b59b275e5ac115
yamato48 <yamato48@YamadaTosiyanoMacBook-Pro.local>
2018-03-21 16:55:19 +0900

    Create edit page
    問４

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,15 @@ class UsersController < ApplicationController
   def edit
   end
   def update
-    if
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
