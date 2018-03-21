b076840706e00eb5e66c9e5823b16cfdca8c5dac
yamato48 <yamato48@YamadaTosiyanoMacBook-Pro.local>
2018-03-21 14:54:52 +0900

    Edit user
    問４まで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if devise controller?
+
+  def configure_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
