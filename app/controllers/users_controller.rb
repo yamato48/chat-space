5917e348ae05663896b6a1a7e9394f960d33e606
yamato48 <yamd1048@gmail.com>
2018-04-03 17:58:23 +0900

    Create form
    controller変更
    jbuilder作成
    Viewにフォーム作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def search
+    @user = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email)
